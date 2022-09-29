11ae9d97a9735fa9815776fc8e4424f807f52cc2
IAm2XTr33M <k.mensen@sintlucasedu.nl>
2022-09-29 10:38:33 +0200

    add the page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}",
+  ],
+  theme: {
+    fontSize:{
+      'mb': ['1vh'],
+    },
+    dropShadow:{
+      'menuShadow': '0 0px 15px rgba(0,0,0,0.6)',
+    }
+  },
+    extend: {
+  },
+  plugins: [],
+}
+
+//  npx tailwindcss -i ./src/style.css -o ./style.css --watch
+
